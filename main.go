9c88f7e6a5b7ce33ae7231309a5dfc7ad7198aad
maxence-ynov <maxencecontact9@gmail.com>
2025-11-21 11:38:16 +0100

    Add URL display feature

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+func UrlHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "You visited: %s", r.URL.Path)
+}
+
 func main() {
+	http.HandleFunc("/", UrlHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
