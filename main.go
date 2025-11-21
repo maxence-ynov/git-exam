8b209016c51571b3eda73ef8b1e1e43df2bbc160
maxence-ynov <maxencecontact9@gmail.com>
2025-11-21 10:44:09 +0100

    Add green color page handler and route

--- main.go
+++ main.go
@@ -14,3 +14,13 @@ func main() {
 	http.HandleFunc("/", UrlHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
+func ColorHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Colors</h1><style>*{background-color: #006400;}</style>")
+}
+
+func main() {
+    ...
+    http.HandleFunc("/color", ColorHandler)
+    ...
+}
